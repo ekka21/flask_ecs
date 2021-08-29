28fe4727fc251b2dfbf3fc4c8d2c034bc9dda99a
Ekkachai Danwanichakul <ek.danwanichakul@baltosoftware.com>
2021-08-29 14:37:43 -0500

    update content

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world, this is a flask app production ready.'
+    return 'Deploying flask_app with github action is super easy!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
